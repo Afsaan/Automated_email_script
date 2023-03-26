6b812217e1e0b8b52d58e7f40d3ab9be37319c7a
harishpanjikar <harishpanjikar@gmail.com>
2023-03-26 07:59:17 +0530

    added impt variables inside .env file and did some minot changes

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 import pandas as pd
 import smtplib
+import os 
+from dotenv import load_dotenv
+import re
+
+def dest_email():
+    while True:
+        receiver_email = input("Please enter the receiver's address:")
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", receiver_email):
+            print('Invalid email address. Please try again.')
+        else:
+            print('Your email is being sent to:', receiver_email)
+
+        return receiver_email
+
+def send_email():
+    sender_email = 'harishpanjikar@gmail.com'
+    sender_pwd = os.environ.get('PASSWORD')
+    receiver_email = dest_email()
+
+    message = "Hey nothing serious. Just testing!"
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(sender_email, sender_pwd)
+        smtp.sendmail(sender_email, receiver_email, message)
+
+def main():
+    load_dotenv()
+    send_email()
+    print('Email sent!')
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 # df = pd.read_csv("mail.csv")
@@ -7,18 +41,9 @@ import smtplib
 # d_list =col.values.tolist()
 # print(d_list)
 
-sender_email = 'harishpanjikar@gmail.com'
-receiver_email = 'harishpanjikar.work@gmail.com'
 
 
-message = "Hey nothing serious. Just testing!"
 
-# def send_email(text):
-#     return "Hey nothing serious. Just testing!"
-
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(sender_email, sender_pwd)
-    smtp.sendmail(sender_email, receiver_email, message)
 
 
 """
