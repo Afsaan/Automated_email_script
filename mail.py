c9abe4dfe219778e16c85dcc611f9969864fbb5f
harishpanjikar <harishpanjikar@gmail.com>
2023-03-31 06:59:39 +0530

    removed print statements and made a constant file. Tried the case scenario where in if the user doesn't have the file they can input email manually but i feel its not dynamic enough. Will need guidance to implement more dynamic code

--- mail.py
+++ mail.py
@@ -1,24 +1,28 @@
 import pandas as pd
 import smtplib
 import logging
-
+from constant import FILE_PATH,SERVER_URL,PORT
 
 def send_email(sender_email,text, sender_pwd):
 
     logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='email_process.log')
+    
+    FILE_PATH = input("enter file name or path:")
 
     try:
-        df = pd.read_csv("mail.csv")
+        df = pd.read_csv(FILE_PATH)
+        column_name = 'email'
+        col = df[column_name]
+        receiver_email =col.values.tolist()
     except FileNotFoundError:
-        print('File not found!')
         logging.critical('File not found')
+        data = input("enter the emails you want the data to send to:")
+        receiver_email = data.split(',')
     except:
-        print(' An error occured while reading the file.')
-    column_name = 'email'
-    col = df[column_name]
-    receiver_email =col.values.tolist()
+        logging.critical(' An error occured while reading the file.')
+    
     logging.info('Read the file')
 
     for emailId in receiver_email:
@@ -30,12 +34,11 @@ def send_email(sender_email,text, sender_pwd):
 
 
         try:
-            with smtplib.SMTP_SSL('smtp.gmail.com', 465, timeout= 10) as smtp:
+            with smtplib.SMTP_SSL(SERVER_URL, PORT, timeout= 10) as smtp:
                 smtp.login(sender_email, sender_pwd)
                 smtp.sendmail(sender_email, emailId , message)
         except Exception as e:
-            print(f'An error occured while sending email. Please try again: {str(e)}')
-            logging.critical(f'An error occured while sending email. Please try again: {str(e)}')
+            logging.critical(f'An error occured while sending email. Please try again: {str(e)}{emailId}')
 
 
 
